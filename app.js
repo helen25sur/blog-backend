4d9bc056d9297a906c3dfe21203c0d8802986324
Olena Surilova <elen21surilova@gmail.com>
2026-01-30 23:56:12 +0100

    feat: add and use User model

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const mongoose = require('mongoose');
 
 // const mongoConnect = require('./db/database').mongoConnect;
 
-// const User = require('./models/user');
+const User = require('./models/user');
 
 const app = express();
 app.use(cors());
@@ -14,22 +14,36 @@ app.use(express.json());
 
 const postsRouter = require('./routes/posts');
 
-// app.use((req, res, next) => {
-//   User.findById('6973e9d3dbfec7a487e5f469')
-//     .then(user => {
-//       req.user = user;
-//       next();
-//     })
-//     .catch(err => {
-//       console.error(err);
-//     })
-// })
+app.use((req, res, next) => {
+  User.findById('6973e9d3dbfec7a487e5f469')
+    .then(user => {
+      if (user) {
+        req.user = user;
+      }
+      next();
+    })
+    .catch(err => {
+      console.error(err);
+    })
+})
 
 app.use('/favicon.ico', express.static('public/favicon.ico'));
 app.use(postsRouter);
 
 mongoose.connect(`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_USER_PASSWORD}@cluster0.uwu6dns.mongodb.net/blog?retryWrites=true&w=majority`)
   .then(result => {
+    User.findOne()
+      .then(user => {
+        if (!user) {
+          const newUser = new User({
+            userName: 'Lena',
+            email: 'test@gmail.com',
+            avatarUrl: 'https://i.ibb.co/Q3fRhbDS/photo-2022-04-04-14-02-12.jpg'
+          })
+          newUser.save();
+        }
+      })
+
     console.log("Connected to DB:", mongoose.connection.name);
     app.listen(3000, () => console.log("Server running on port 3000"));
   })
--- models/user.js
+++ models/user.js
@@ -1,36 +1,57 @@
-const mongodb = require('mongodb');
+const mongoose = require('mongoose');
 
-const getDB = require('../db/database').getDB;
+const Schema = mongoose.Schema;
 
-class User {
-  constructor(userName, email, avatarUrl) {
-    this.userName = userName;
-    this.email = email;
-    this.avatarUrl = avatarUrl;
+const userSchema = new Schema({
+  userName: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  avatarUrl: {
+    type: String,
+    required: true
   }
+});
 
-  save() {
-    const db = getDB();
-    return db.collection('users').insertOne(this)
-      .then(result => {
-        console.log('16 User', result);
-      })
-      .catch(err => {
-        console.error(err);
-      })
-  }
+module.exports = mongoose.model('User', userSchema);
 
-  static findById(userId) {
-    const db = getDB();
-    return db.collection('users').findOne({ _id: new mongodb.ObjectId(userId) })
-      .then(user => {
-        console.log(user);
-        return user;
-      })
-      .catch(err => {
-        console.error(err);
-      })
-  }
-}
+// const mongodb = require('mongodb');
 
-module.exports = User;
+// const getDB = require('../db/database').getDB;
+
+// class User {
+//   constructor(userName, email, avatarUrl) {
+//     this.userName = userName;
+//     this.email = email;
+//     this.avatarUrl = avatarUrl;
+//   }
+
+//   save() {
+//     const db = getDB();
+//     return db.collection('users').insertOne(this)
+//       .then(result => {
+//         console.log('16 User', result);
+//       })
+//       .catch(err => {
+//         console.error(err);
+//       })
+//   }
+
+//   static findById(userId) {
+//     const db = getDB();
+//     return db.collection('users').findOne({ _id: new mongodb.ObjectId(userId) })
+//       .then(user => {
+//         console.log(user);
+//         return user;
+//       })
+//       .catch(err => {
+//         console.error(err);
+//       })
+//   }
+// }
+
+// module.exports = User;
