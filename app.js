267b730efffc6104a5b25201969e211a41d76124
Olena Surilova <elen21surilova@gmail.com>
2026-01-30 22:20:58 +0100

    feat: add mongoose

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
+require('dotenv').config();
+
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
 
-const mongoConnect = require('./db/database').mongoConnect;
+// const mongoConnect = require('./db/database').mongoConnect;
 
 const User = require('./models/user');
 
@@ -25,6 +28,10 @@ app.use((req, res, next) => {
 app.use('/favicon.ico', express.static('public/favicon.ico'));
 app.use(postsRouter);
 
-mongoConnect(() => {
-  app.listen(3000, () => console.log("Server running on port 3000"));
-});
+mongoose.connect(`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_USER_PASSWORD}@cluster0.uwu6dns.mongodb.net/blog?retryWrites=true&w=majority`)
+  .then(result => {
+    app.listen(3000, () => console.log("Server running on port 3000"));
+  })
+  .catch(err => {
+    console.error(err);
+  })
