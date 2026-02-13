3657266e34337736f528c2064a276c9eb57491d6
Olena Surilova <elen21surilova@gmail.com>
2026-02-13 23:05:45 +0100

    feat: start working with auth

---
+++ controllers/auth.js
@@ -0,0 +1,10 @@
+exports.getLogin = (req, res, next) => {
+  res.json({ message: "Login route" });
+};
+
+exports.postLogin = (req, res, next) => {
+  const { username, password } = req.body;
+  // Here you would typically check the username and password against your database
+  // For demonstration, we'll just return a success message
+  res.json({ message: "Login successful", username });
+}
---
+++ routes/auth.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+
+const authControllers = require('../controllers/auth');
+
+router.get('/login', authControllers.getLogin);
+
+router.post('/login', authControllers.postLogin);
+
+module.exports = router;
