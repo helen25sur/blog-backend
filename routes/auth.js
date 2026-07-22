ee8e2676d7f4bcf0dc21cba8531315fbc4db1d59
Olena Surilova <elen21surilova@gmail.com>
2026-07-22 15:25:43 +0200

    feat: add getStatus into controllers/auth

--- routes/auth.js
+++ routes/auth.js
@@ -3,6 +3,8 @@ const router = express.Router();
 
 const authControllers = require('../controllers/auth');
 
+router.get('/status', authControllers.getStatus);
+
 router.get('/login', authControllers.getLogin);
 
 router.post('/login', authControllers.postLogin);
