059e13d30f4331f56e790b9e0ec265fbb6eea0ac
Olena Surilova <elen21surilova@gmail.com>
2026-07-22 17:21:02 +0200

    fix: edit getStatus and postLogin into controllers/auth

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,9 +1,21 @@
 const User = require('../models/user');
 
 exports.getStatus = (req, res) => {
+  console.log(req.session);
+  const sessionUser = req.session.user;
+
+  const user = sessionUser
+    ? {
+      _id: sessionUser._id.toString(),
+      userName: sessionUser.userName,
+      email: sessionUser.email,
+      avatarUrl: sessionUser.avatarUrl
+    }
+    : null;
+
   res.json({
     isAuthenticated: !!req.session.isLoggedIn,
-    user: req.session.user || null
+    user
   });
 };
 
@@ -15,20 +27,27 @@ exports.postLogin = (req, res, next) => {
   const { username, password } = req.body;
 
   User.findById('6973e9d3dbfec7a487e5f469')
-    .then(user => {
-      if (user) {
-        req.session.isLoggedIn = true;
-        req.session.user = user;
-        // Here you would typically check the username and password against your database
-        // For demonstration, we'll just return a success message
-        res.json({ message: "Login successful", username });
+    .then(userFound => {
+      if (!userFound) {
+        return res.status(404).json({ message: 'User not found' });
       }
 
+      req.session.isLoggedIn = true;
+      req.session.user = {
+        _id: userFound._id.toString(),
+        userName: userFound.userName,
+        email: userFound.email,
+        avatarUrl: userFound.avatarUrl
+      };
+      // Here you would typically check the username and password against your database
+      // For demonstration, we'll just return a success message
+      res.json({ message: "Login successful", username });
     })
     .catch(err => {
       console.error(err);
-    })
-}
+      res.status(500).json({ message: err.message });
+    });
+};
 
 exports.postLogout = (req, res, next) => {
   req.session.destroy((err) => {
